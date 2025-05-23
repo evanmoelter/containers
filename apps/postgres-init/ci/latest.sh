ffc8bbc3e5c58e8ca898b35ac7bf8396928214bc
Evan Moelter <2207348+evanmoelter@users.noreply.github.com>
2025-05-23 11:32:36 -0700

    Fix issue with multiline regex

--- apps/postgres-init/ci/latest.sh
+++ apps/postgres-init/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -s "https://dl-cdn.alpinelinux.org/alpine/v3.19/main/x86_64/APKINDEX.tar.gz" | tar -xzO 2>/dev/null | grep -P "P:postgresql16-client\n(.*\n)*?V:\K[^\n]+" --only-matching)
+version=$(curl -s "https://dl-cdn.alpinelinux.org/alpine/v3.19/main/x86_64/APKINDEX.tar.gz" | tar -xzO 2>/dev/null | awk 'BEGIN{RS=""; ORS="\n\n"} {gsub(/\n/, " "); print}' | grep --text -P "P:postgresql16-client(.*)V:\K[^\s]+" --only-matching)
 version="${version%%_*}"
 version="${version%%-*}"
 printf "%s" "${version}"
